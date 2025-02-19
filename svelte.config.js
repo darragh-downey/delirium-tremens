eab95fd592a1f81c3050f63332e061a4ab5cf29f
Darragh Downey <darragh.downey90@gmail.com>
2025-02-19 14:24:26 +1100

    fix: base path and prerender entries

--- svelte.config.js
+++ svelte.config.js
@@ -14,32 +14,31 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/book' : ''
+			base: process.env.GITHUB_ACTIONS ? '/critical-infra/book' : ''
 		},
 		prerender: {
 			entries: [
-				'*',
-				'/',
-				'/authors',
-				'/01-introduction',
-				'/02-gold-trader',
-				'/03-siege-engines',
-				'/04-fortress-architect',
-				'/05-weak-points',
-				'/06-inner-keep',
-				'/07-moat-drawbridge',
-				'/08-reconnaissance',
-				'/09-simulated-sieges',
-				'/10-reinforcing-walls',
-				'/11-gatekeepers-watch',
-				'/12-guarding-granary',
-				'/13-watchtower-alert',
-				'/14-armorers-duty',
-				'/15-architects-blueprint',
-				'/16-training-garrison',
-				'/17-new-age',
-				'/18-fortifying-future',
-				'/references'
+				'/book',
+				'/book/authors',
+				'/book/01-introduction',
+				'/book/02-gold-trader',
+				'/book/03-siege-engines',
+				'/book/04-fortress-architect',
+				'/book/05-weak-points',
+				'/book/06-inner-keep',
+				'/book/07-moat-drawbridge',
+				'/book/08-reconnaissance',
+				'/book/09-simulated-sieges',
+				'/book/10-reinforcing-walls',
+				'/book/11-gatekeepers-watch',
+				'/book/12-guarding-granary',
+				'/book/13-watchtower-alert',
+				'/book/14-armorers-duty',
+				'/book/15-architects-blueprint',
+				'/book/16-training-garrison',
+				'/book/17-new-age',
+				'/book/18-fortifying-future',
+				'/book/references'
 			],
 			handleHttpError: ({ path, referrer, message }) => {
 				// Ignore all 404s during prerendering
